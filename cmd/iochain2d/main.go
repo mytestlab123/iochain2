c215683023c363bed9bbc6a5c31e4fff02eecafc
Developer Experience team at Tendermint <hello@tendermint.com>
2022-11-08 06:30:59 +0000

    Initialized with Ignite CLI

---
+++ cmd/iochain2d/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/mytestlab123/iochain2/app"
+	"github.com/mytestlab123/iochain2/cmd/iochain2d/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
+	}
+}
